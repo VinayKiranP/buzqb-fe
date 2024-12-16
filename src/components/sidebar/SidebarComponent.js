8ad930657f90c0ee9eaaafb3d9ddeb238f7fbf25
ವಿನಯ್ ಕಿರಣ್ ಪಾಟೀಲ ( Vinay Kiran P) <vinaykiranp888@gmail.com>
2024-12-17 02:38:36 +0530

    sidebar and menus builded

--- src/components/sidebar/SidebarComponent.js
+++ src/components/sidebar/SidebarComponent.js
@@ -4,11 +4,13 @@ import MenuListComponent from '../sidebar/MenuListComponent';
 import ToggleThemeButtonComponent from '../sidebar/ToggleThemeButtonComponent';
 import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import Logo from "../../Assets/Logo.svg";
+import NavbarComponent from '../web/NavbarComponent';
+import ContactComponent from '../web/ContactComponent';
 
 const { Header, Sider } = Layout;
 const SidebarComponent = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const [ darkTheme, setDarkTheme ] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(false);
   const toggleTheme = () => {
     setDarkTheme(!darkTheme);
   }
@@ -23,7 +25,7 @@ const SidebarComponent = () => {
         collapsed={collapsed}
         collapsible
         trigger={null}
-        className="sidebar" 
+        className="sidebar"
         theme={ darkTheme ? 'dark' : 'light'}>
           <div className="logo">
             <img src={Logo} alt="Home" className="logo-icon"/>
@@ -31,14 +33,18 @@ const SidebarComponent = () => {
           <MenuListComponent darkTheme={darkTheme}/>
           <ToggleThemeButtonComponent darkTheme={darkTheme} toggleTheme={toggleTheme}/>
         </Sider>
-        <Layout>
+        <Layout theme={ darkTheme ? 'dark' : 'light'}>
           <Header style={{padding:0, background:colorBgContainer}}>
+            <NavbarComponent/>
             <AntButton 
             type="text" 
             className="toggle" 
             onClick={() => setCollapsed(!collapsed)}
             icon={ collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}/>
           </Header>
+          <div className="">
+            <ContactComponent />
+          </div>
         </Layout>
       </Layout>
   )
